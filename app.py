555d4010225709cdbb608a9dad4f217bc6b7c3d5
Sujalkarale45 <karalesr_1@rknec.edu>
2025-01-25 10:33:20 +0530

    "My First Render_Template "

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
+
+@app.route("/AboutUs")
+def about():
+    return "<p>My Name is Sujal</p>"
+
+@app.route("/Login")
+def login():
+    return "<p>Login Page</p>"
+
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
